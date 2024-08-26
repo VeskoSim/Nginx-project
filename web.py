91776518d8f800f95ea4e20f5d0f304490bce1dd
VeskoSim <wsimeonow@abv.bg>
2024-08-26 22:13:05 +0300

    added changes

--- web.py
+++ web.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-env=os.getenv('ENV')
+# Променлива на околната среда
+env = os.getenv('ENV', 'Development')
 
 @app.route("/")
 def home():
-    return f"Hello from {env}"
+    # Променяме съобщението за тестване
+    return f"Hello from {env}! This is the updated version."
 
 @app.route("/picture")
 def pic():
+    # Зарежда шаблона index.html
     return render_template('index.html')
 
 if __name__ == "__main__":
-         app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
